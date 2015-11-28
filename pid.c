de37b46029d8a71ce620fa21d69986b3d246caad
suhussai <suhussai@ualberta.ca>
2015-11-28 12:18:30 -0700

    fixed issues and errors with calculation

--- pid.c
+++ pid.c
@@ -14,8 +14,8 @@
 #include <time.h>
 
 #define P 1
-#define I 0.01
-#define D 1
+#define I 0.1
+#define D 10
 
 float accumulated_error = 0;
 float time_called_previously = 0;
@@ -35,7 +35,7 @@ float PID(float currentTemp, float setPoint) {
 
   // integral part
   time_passed = seconds - time_passed;
-  accumulated_error = (currentTemp - accumulated_error)*(time_passed) + accumulated_error;
+  accumulated_error = (currentTemp - past_temperature)*(time_passed) + accumulated_error;
   float i_value = accumulated_error * I;
   
   float d_value = 0;
@@ -43,7 +43,7 @@ float PID(float currentTemp, float setPoint) {
   d_value = d_value * D;
 
   time_passed = seconds;
-
+  past_temperature = currentTemp;
   printf("p_value = %f, i_value = %f, d_value = %f\n", p_value, i_value, d_value);
   return p_value + i_value + d_value;
   
@@ -55,10 +55,16 @@ int main(int argc, char argv[]) {
   int i = 0;
   float setPoint = 75.00;
   time_passed = time(NULL);
-  for (i = 0; i < 10; i++) {
+  for (i = 0; i < 15; i++) {
     sleep(2);
     printf("currentTemp = %f, setPoint = %f, fanSpeed = %f \n", (float)(i + 60), setPoint, PID((float)(i + 60), setPoint));
+  }
+
+  for (i = 0; i < 15; i++) {
+    sleep(2);
+    printf("currentTemp = %f, setPoint = %f, fanSpeed = %f \n", (float)(74 - i), setPoint, PID((float)(74 - i), setPoint));
   }  
+
 
   return 0;
 }
